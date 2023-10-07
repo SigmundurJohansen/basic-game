3554218eb7c6d6db349dc61194e02b13c37f6c3c
SigmundurJohansen <nebuchanezher@hotmail.com>
2023-10-07 02:16:59 +0200

    update links to engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,164 +1,172 @@
-#include <iostream>
-#include "monster.h"
+#include "../AvariceEngine/entrypoint.h"
 #include "humanoid.h"
-#include <../AvariceEngine/entrypoint.h>
-#include <coroutine>
+#include "monster.h"
+#include <iostream>
 
 using namespace Avarice;
 
-class SuperGame : public Game
-{
+class SuperGame : public Game {
 public:
-    Humanoid all_humans[100];
-    InputManager *m_inputManager;
-    static inline bool m_orientate;
-    static inline bool m_moveUp;
-    static inline bool m_moveLeft;
-    static inline bool m_moveRight;
-    static inline bool m_moveDown;
-    static inline bool m_moveForward;
-    static inline bool m_moveBackward;
+  Humanoid all_humans[100];
+  InputManager *m_inputManager;
+  static bool m_orientate;
+  static bool m_moveUp;
+  static bool m_moveLeft;
+  static bool m_moveRight;
+  static bool m_moveDown;
+  static bool m_moveForward;
+  static bool m_moveBackward;
 
-    static void ToggleOrientate(){ m_orientate ? m_orientate = false : m_orientate = true;}
-    static void ToggleMoveUp(){ m_moveUp ? m_moveUp = false : m_moveUp = true;}
-    static void ToggleMoveLeft(){ m_moveLeft ? m_moveLeft = false : m_moveLeft = true;}
-    static void ToggleMoveRight(){ m_moveRight ? m_moveRight = false : m_moveRight = true;}
-    static void ToggleMoveDown(){ m_moveDown ? m_moveDown = false : m_moveDown = true;}
-    static void ToggleMoveForward(){ m_moveForward ? m_moveForward = false : m_moveForward = true;}
-    static void ToggleMoveBack(){ m_moveBackward ? m_moveBackward = false : m_moveBackward = true;}
+  static void ToggleOrientate() {
+    m_orientate ? m_orientate = false : m_orientate = true;
+  }
+  static void ToggleMoveUp() { m_moveUp ? m_moveUp = false : m_moveUp = true; }
+  static void ToggleMoveLeft() {
+    m_moveLeft ? m_moveLeft = false : m_moveLeft = true;
+  }
+  static void ToggleMoveRight() {
+    m_moveRight ? m_moveRight = false : m_moveRight = true;
+  }
+  static void ToggleMoveDown() {
+    m_moveDown ? m_moveDown = false : m_moveDown = true;
+  }
+  static void ToggleMoveForward() {
+    m_moveForward ? m_moveForward = false : m_moveForward = true;
+  }
+  static void ToggleMoveBack() {
+    m_moveBackward ? m_moveBackward = false : m_moveBackward = true;
+  }
 
-    void SetGameSettings()
-    {
-        m_moveUp = false;
-        m_inputManager = ServiceLocator::GetInputManager();
-        if(m_inputManager)
-        {
-            std::cout << "setting input manager \n";
-            m_inputManager->MapInputToAction(InputKey::MOUSE_LEFT, InputAction{ .actionName = "click" });
-            m_inputManager->MapInputToAction(InputKey::KEY_W, InputAction{ .actionName = "forward" });
-            m_inputManager->MapInputToAction(InputKey::KEY_A, InputAction{ .actionName = "left" });
-            m_inputManager->MapInputToAction(InputKey::KEY_S, InputAction{ .actionName = "back" });
-            m_inputManager->MapInputToAction(InputKey::KEY_D, InputAction{ .actionName = "right" });
-            m_inputManager->MapInputToAction(InputKey::KEY_C, InputAction{ .actionName = "down" });
-            m_inputManager->MapInputToAction(InputKey::SPACE, InputAction{ .actionName = "up" });
+  void SetGameSettings() {
+    m_moveUp = false;
+    m_inputManager = ServiceLocator::GetInputManager();
+    if (m_inputManager) {
+      std::cout << "setting input manager \n";
+      m_inputManager->MapInputToAction(InputKey::MOUSE_LEFT,
+                                       InputAction{.actionName = "click"});
+      m_inputManager->MapInputToAction(InputKey::KEY_W,
+                                       InputAction{.actionName = "forward"});
+      m_inputManager->MapInputToAction(InputKey::KEY_A,
+                                       InputAction{.actionName = "left"});
+      m_inputManager->MapInputToAction(InputKey::KEY_S,
+                                       InputAction{.actionName = "back"});
+      m_inputManager->MapInputToAction(InputKey::KEY_D,
+                                       InputAction{.actionName = "right"});
+      m_inputManager->MapInputToAction(InputKey::KEY_C,
+                                       InputAction{.actionName = "down"});
+      m_inputManager->MapInputToAction(InputKey::SPACE,
+                                       InputAction{.actionName = "up"});
 
-            m_inputManager->RegisterActionCallback("left", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+      m_inputManager->RegisterActionCallback(
+          "left",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 std::cout << "left";
                 ToggleMoveLeft();
                 return true;
-            }
-            });
+              }});
 
-            m_inputManager->RegisterActionCallback("right", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+      m_inputManager->RegisterActionCallback(
+          "right",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 std::cout << "right";
                 ToggleMoveRight();
                 return true;
-            }
-            });
+              }});
 
-            m_inputManager->RegisterActionCallback("back", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+      m_inputManager->RegisterActionCallback(
+          "back",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 std::cout << "backingup";
                 ToggleMoveBack();
                 return true;
-            }
-            });
+              }});
 
-            m_inputManager->RegisterActionCallback("forward", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+      m_inputManager->RegisterActionCallback(
+          "forward",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 ToggleMoveForward();
                 return true;
-            }
-            });
+              }});
 
-            m_inputManager->RegisterActionCallback("down", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+      m_inputManager->RegisterActionCallback(
+          "down",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 std::cout << "down";
                 ToggleMoveDown();
                 return true;
-            }
-            });
-            m_inputManager->RegisterActionCallback("up", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [] (InputSource source, int sourceIndex, float value)
-            {
+              }});
+      m_inputManager->RegisterActionCallback(
+          "up",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [](InputSource source, int sourceIndex, float value) {
                 ToggleMoveUp();
                 return true;
-            }
-            });
-            m_inputManager->RegisterActionCallback("click", InputManager::ActionCallback { 
-                .Ref = "main game",
-                .Func = [this] (InputSource source, int sourceIndex, float value)
-            {
+              }});
+      m_inputManager->RegisterActionCallback(
+          "click",
+          InputManager::ActionCallback{
+              .Ref = "main game",
+              .Func = [this](InputSource source, int sourceIndex, float value) {
                 std::cout << "clicking";
-                if(value == 0)                
-                    ServiceLocator::GetCamera()->ToggleFirstMouseClick();
-                
+                if (value == 0)
+                  ServiceLocator::GetCamera()->ToggleFirstMouseClick();
+
                 ServiceLocator::GetWindow()->SetCursorVisibility(false);
                 ToggleOrientate();
-                if(value == 0)
-                    ServiceLocator::GetWindow()->SetCursorVisibility(true);
+                if (value == 0)
+                  ServiceLocator::GetWindow()->SetCursorVisibility(true);
                 return true;
-            }
-            });
-        }
+              }});
     }
+  }
 
-    explicit SuperGame(std::string _title) : Game(std::move(_title))
-    {
-        SetGameSettings();
-        all_humans[0].m_name = "billy";
-        all_humans[0].PrintStat();
-        all_humans[1].PrintStat();
-        Item longswordPlussOne;
-        longswordPlussOne.m_name = "longsword +1";
-        longswordPlussOne.AddEnchantment(ItemEnchantment(damage, 1));
-        longswordPlussOne.AddEnchantment(ItemEnchantment(thac0, 1));
-        all_humans[0].m_inventory[6] = longswordPlussOne;
-        std::cout << "equpied weapon: ";
-        all_humans[0].m_inventory[6].PrintStats();
-        Monster one("goblin");
-        std::cout << one.GetName() << std::endl;
-    }
+  explicit SuperGame(std::string _title) : Game(_title) {
+    SetGameSettings();
+    all_humans[0].m_name = "billy";
+    all_humans[0].PrintStat();
+    all_humans[1].PrintStat();
+    Item longswordPlussOne;
+    longswordPlussOne.m_name = "longsword +1";
+    longswordPlussOne.AddEnchantment(ItemEnchantment(damage, 1));
+    longswordPlussOne.AddEnchantment(ItemEnchantment(thac0, 1));
+    all_humans[0].m_inventory[6] = longswordPlussOne;
+    std::cout << "equpied weapon: ";
+    all_humans[0].m_inventory[6].PrintStats();
+    Monster one("goblin");
+    std::cout << one.GetName() << std::endl;
+  }
 
 protected:
-    virtual void Update(float _deltaTime) override
-    {
-        if(m_orientate)
-        {
-            ServiceLocator::GetCamera()->Orientate(_deltaTime);
-        }
-        if(m_moveUp)
-            ServiceLocator::GetCamera()->MoveUp(_deltaTime);
-        if(m_moveDown)
-            ServiceLocator::GetCamera()->MoveDown(_deltaTime);
-        if(m_moveLeft)
-            ServiceLocator::GetCamera()->MoveLeft(_deltaTime);
-        if(m_moveRight)
-            ServiceLocator::GetCamera()->MoveRight(_deltaTime);
-        if(m_moveForward)
-            ServiceLocator::GetCamera()->MoveForward(_deltaTime);
-        if(m_moveBackward)
-            ServiceLocator::GetCamera()->MoveBackwards(_deltaTime);
-
-            
-        //std::cout << "i'm updating" << std::endl;
+  virtual void Update(float _deltaTime) override {
+    if (m_orientate) {
+      ServiceLocator::GetCamera()->Orientate(_deltaTime);
     }
+    if (m_moveUp)
+      ServiceLocator::GetCamera()->MoveUp(_deltaTime);
+    if (m_moveDown)
+      ServiceLocator::GetCamera()->MoveDown(_deltaTime);
+    if (m_moveLeft)
+      ServiceLocator::GetCamera()->MoveLeft(_deltaTime);
+    if (m_moveRight)
+      ServiceLocator::GetCamera()->MoveRight(_deltaTime);
+    if (m_moveForward)
+      ServiceLocator::GetCamera()->MoveForward(_deltaTime);
+    if (m_moveBackward)
+      ServiceLocator::GetCamera()->MoveBackwards(_deltaTime);
+
+    // std::cout << "i'm updating" << std::endl;
+  }
 };
 
-Game *Avarice::CreateGame()
-{
-    return new SuperGame("main game");
-}
+Game *Avarice::CreateGame() { return new SuperGame("main game"); }
